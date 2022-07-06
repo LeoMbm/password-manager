716fdaa6c0ce0f7ce78eab47c855f05c4690fb75
LeoMbm <leonidas.j.mbm@gmail.com>
2022-07-07 00:14:24 +0200

    fix: check password look in db

--- main.py
+++ main.py
@@ -1,98 +1,141 @@
 # Firstly, I imported the module for psycopg
-import argparse
 from utils import dbconfig
 import sys
 import bcrypt
 import dotenv
 import psycopg2
+from getpass import getpass, getuser
 
 from simple_term_menu import TerminalMenu
 
 dotenv.load_dotenv()
-my_parser = argparse.ArgumentParser(description="Password Manager: Create, Add and Delete account", usage="[options]")
+
+# # Get Password & Encrypt
+# master_secret_key = getpass()
+# salt = bcrypt.gensalt()
+# pw = bcrypt.hashpw(master_secret_key.encode(), salt)
+# ## Username & Password needs to be stored somewhere
+# # For checking future logins
+# ## `pw` would be retrieved from a db, I would prefer keyring APIs
+# password_prompt = getpass()
+# if not bcrypt.checkpw(password_prompt.encode(), pw):
+#     print('Fail')
+# else:
 
 main_menu = ["[A] Create new user", "[B] Log in", "[Q] Exit"]
-login_menu = ["[A] Create new data", "[B] View your data", "[C] Choice one data by site"]
+login_menu = ["[A] Create new data", "[B] View your data", "[C] Choice one data by site", "[Q] Exit"]
 loop = True
+conn = dbconfig.dbconfig()
+cur = conn.cursor()
 while loop:
+
     choice = main_menu[TerminalMenu(main_menu, title="Main menu").show()]
+    if choice == "[Q] Exit":
+        sys.exit()
+    if choice == "[A] Create new user":
+        def register_user(username, password):
+            try:
 
+                salt = bcrypt.gensalt()
+                pw = bcrypt.hashpw(password.encode(), salt)
+                query = "INSERT INTO users (username, master_key) values (%s, %s);"
+                cur.execute(query, (username, pw))
+                conn.commit()
+                # cursor.close()
+                conn.close()
+            except Exception as error:
+                print(error)
+
+
+        master_username = input('Type your Username: ')
+        master_password = getpass()
+        register_user(master_username, master_password)
     if choice == "[B] Log in":
-        master_password = input("What's your master password: ")
-
-        if master_password != "1810":
-            print("Login fail")
-            sys.exit()
-        else:
-            print('Welcome in the password manager')
-            connection = dbconfig.dbconfig()
-            cursor = connection.cursor()
-
-            while loop:
-                choice = login_menu[TerminalMenu(login_menu, title="Login menu").show()]
-                if choice == "[A] Create new data":
-
-                    def store_password(usrnm, pwd, url):
-                        try:
-                            # TODO: WHAT IS CURSOR ?
-                            query = "INSERT INTO password (username, password, site) values (%s, %s, %s);"
-                            cursor.execute(query, (usrnm, pwd, url))
-                            cursor.execute("select * from password;")
-                            rows = cursor.fetchall()
-                            for r in rows:
-                                print(r[0], r[1], r[2], r[3])
-                            connection.commit()
-                            # cursor.close()
-                            connection.close()
-                            # TODO:
-                        except(Exception, psycopg2.Error) as error:
-                            print(error)
-
-
-                    def make_password(password):
-                        pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-                        print(str(pw))
-                        return pw
-
-
-                    username_input = input("Type your username/email: ")
-                    password_input = input("Type your password: ")
-                    site_input = input("Type the site: ")
-
-                    store_password(str(username_input), make_password(str(password_input)), str(site_input))
-                if choice == "[B] View your data":
-                    def get_data():
-                        try:
-                            query = "SELECT * FROM password;"
-                            cursor.execute(query)
-                            rows = cursor.fetchall()
-                            for r in rows:
-                                print(r[0], r[1], r[2], r[3])
-                        except(Exception, psycopg2.Error) as error:
-                            print(error)
-
-
-                    get_data()
-                if choice == "[C] Choice one data by site":
-                    site_input = str(input("Type the site: "))
-
-
-                    def get_one_data(url):
-                        try:
-                            query = f"SELECT * FROM password WHERE site='{url}';"
-                            cursor.execute(query)
-                            rows = cursor.fetchall()
-                            for r in rows:
-                                print(r[0], r[1], r[2], r[3])
-
-                        except(Exception, psycopg2.Error) as error:
-                            print(error)
-
-
-                    get_one_data(str(site_input))
-                if choice == "[Q] Exit":
-                    loop = False
+        def log_in(usnm, pw):
+            query = f"SELECT * from users WHERE (master_key='{pw}' );"
+            cur.execute(query)
+            rows = cur.fetchall()
+            for r in rows:
+                print(r[0], r[1], r[2])
+                if bcrypt.checkpw(pw, str(r[2])):
+                    # FIXME: THIS 'IF'DO THINGS WRONG
+                    print('Welcome in the password manager')
+                else:
+                    print("Login fail or user doesnt exist")
                     sys.exit()
+
+            # FIXME: HE DOESNT CHECK THE PASSWORD, ANY PASSWORD WORKS
+
+
+        master_un = input("What's your username: ")
+        master_pw = str(getpass())
+
+        log_in(master_un, master_pw)
+while loop:
+    choice = login_menu[TerminalMenu(login_menu, title="Login menu").show()]
+    if choice == "[A] Create new data":
+        def store_password(usrnm, pwd, url):
+            try:
+                # TODO: WHAT IS CURSOR ?
+                req = "INSERT INTO password (username, password, site) values (%s, %s, %s);"
+                cur.execute(req, (usrnm, pwd, url))
+                cur.execute("select * from password;")
+                # TODO: STORE PASSWORD WHERE ID = USERNAME
+                rows = cur.fetchall()
+                for r in rows:
+                    print(r[0], r[1], r[2], r[3])
+                conn.commit()
+                # cursor.close()
+                conn.close()
+                # TODO:
+            except(Exception, psycopg2.Error) as error:
+                print(error)
+
+
+        def make_password(password):
+            pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            print(str(pw))
+            return pw
+
+
+        username_input = input("Type your username/email: ")
+        password_input = input("Type your password: ")
+        site_input = input("Type the site: ")
+
+        store_password(str(username_input), make_password(str(password_input)), str(site_input))
+    if choice == "[B] View your data":
+        def get_data():
+            try:
+                que = "SELECT * FROM password;"
+                cur.execute(que)
+                row = cur.fetchall()
+                for i in row:
+                    print(i[1], i[2], i[3])
+            except(Exception, psycopg2.Error) as error:
+                print(error)
+
+
+        get_data()
+    if choice == "[C] Choice one data by site":
+        site_input = str(input("Type the site: "))
+
+
+        def get_one_data(url):
+            try:
+                query = f"SELECT * FROM password WHERE site='{url}';"
+                cur.execute(query)
+                row = cur.fetchall()
+                for i in row:
+                    print(i[0], i[1], i[2], i[3])
+
+            except(Exception, psycopg2.Error) as error:
+                print(error)
+
+
+        get_one_data(str(site_input))
+
+    if choice == "[Q] Exit":
+        sys.exit()
 
 # TODO: IMPLEMENT LOGIN WITH MASTER PASSWORD
 
